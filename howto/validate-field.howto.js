6adaeb7640e28f4736de13914192fa6996a94616
Hajime Yamasaki Vukelic <hi@devi.us>
2023-06-11 23:35:31 +0200

    Fix field validation logic

--- howto/validate-field.howto.js
+++ howto/validate-field.howto.js
@@ -1,5 +1,5 @@
 export default function validateField(validate, getMessage, $field) {
-	if ($field.value && validate($field.value)) {
+	if ($field.value == '' || validate($field.value)) {
 		$field.setCustomValidity('')
 		return true
 	} else {
